87a382a9dce7b5ad0894b789d7281a18339f230c
MK <mikalkimmani@gmail.com>
2024-03-11 22:31:56 +0300

    master: Point to create user hanlder

---
+++ handler_user.go
@@ -0,0 +1,73 @@
+package main
+
+import (
+	"database/sql"
+	"encoding/json"
+	"fmt"
+	"net/http"
+	"time"
+
+	"github.com/google/uuid"
+	"github.com/guregu/null"
+	"github.com/mike-kimani/rssagg/internal/database"
+)
+
+type jsonNullInt32 struct{
+	sql.NullInt32
+}
+
+func(v jsonNullInt32) MarshalJSON() ([]byte, error) {
+	if v.Valid {
+		return json.Marshal(v.Int32)
+	} else {
+		return json.Marshal(v.Int32)
+	}
+
+}
+
+func(v *jsonNullInt32) UnmarshalJSON(data []byte) error {
+	var x *int32
+	err := json.Unmarshal(data, &x)
+	
+	if err != nil {
+		return err
+	}
+	if x != nil {
+		v.Valid = true
+		v.Int32 = *x
+	} else {
+		v.Valid = false
+	}
+	return nil
+}
+
+func (apiCfg *apiConfig)handlerCreateUser(w http.ResponseWriter, r *http.Request) {
+
+	type parameters struct{
+		Name string `json:"name"`
+		ChickenBought sql.NullInt32 `json:"chicken_bought"`
+	}
+
+	decoder := json.NewDecoder(r.Body)
+
+	params := parameters{}
+	err := decoder.Decode(&params)
+	if err != nil {
+		respondWithError(w, 400, fmt.Sprintf("Error parsing json: %v", err))
+		return
+	}
+
+	user, err:= apiCfg.DB.CreateUser(r.Context(),database.CreateUserParams{
+		ID: uuid.New(),
+		CreatedAt: time.Now().UTC(),
+		UpdatedAt: time.Now().UTC(),
+		Name: params.Name,
+		ChickenBought: params.ChickenBought,
+	} )
+	if err != nil {
+		respondWithError(w, 400, fmt.Sprintf("Couldn't create user: %v", err))
+		return
+	}
+
+	respondWithJson(w, 200, databaseUserToUser(user))
+}
