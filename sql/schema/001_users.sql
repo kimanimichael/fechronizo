49dfb18614c6740379f7ca81fbb20c472cfab83b
MK <mikalkimmani@gmail.com>
2024-03-12 05:12:50 +0300

    master: Add column to sql migrations

--- sql/schema/001_users.sql
+++ sql/schema/001_users.sql
@@ -4,7 +4,8 @@ CREATE TABLE users (
     id UUID PRIMARY KEY,
     created_at TIMESTAMP NOT NULL,
     updated_at TIMESTAMP NOT NULL,
-    name TEXT NOT NULL
+    name TEXT NOT NULL,
+    chicken_bought INT
 );
 
 -- +goose Down
