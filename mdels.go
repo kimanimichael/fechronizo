9279047b2b48159385b6d7490f053499511a1e94
MK <mikalkimmani@gmail.com>
2024-03-12 05:11:34 +0300

    master: Cast database user to our own user

---
+++ mdels.go
@@ -0,0 +1,28 @@
+package main
+
+import (
+	"time"
+
+	"github.com/google/uuid"
+	"github.com/mike-kimani/rssagg/internal/database"
+
+	"database/sql"
+)
+
+type User struct {
+	ID        uuid.UUID `json:"id"`
+	CreatedAt time.Time `json:"created_at"`
+	UpdatedAt time.Time `json:"updated_at"`
+	Name      string `json:"name"`
+	ChickenBought sql.NullInt32 `json:"chicken_bought"`
+}
+
+func databaseUserToUser(dbUser database.User) User{
+	return User{
+		ID: dbUser.ID,
+		CreatedAt: dbUser.CreatedAt,
+		UpdatedAt: dbUser.UpdatedAt,
+		Name: dbUser.Name,
+		ChickenBought: dbUser.ChickenBought,
+	}
+}
