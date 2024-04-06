c37bb75f36b595cc9d941951c6d5038a6b7500d7
MK <mikalkimmani@gmail.com>
2024-04-06 03:29:11 +0300

    (master): Customized Feed from DatabaseFeed

--- mdels.go
+++ mdels.go
@@ -28,3 +28,26 @@ func databaseUserToUser(dbUser database.User) User{
 		APIKey: dbUser.ApiKey,
 	}
 }
+
+
+type Feed struct {
+	ID        uuid.UUID `json:"id"`
+	CreatedAt time.Time `json:"created_at"`
+	UpdatedAt time.Time `json:"updated_at"`
+	Name      string `json:"name"`
+	URL string `json:"url"`
+	UserID uuid.UUID `json:"user_id"`
+	UserName string `json:"user_name"`
+}
+
+func databaseUserToFeed(dbFeed database.Feed) Feed{
+	return Feed{
+		ID: dbFeed.ID,
+		CreatedAt: dbFeed.CreatedAt,
+		UpdatedAt: dbFeed.UpdatedAt,
+		Name: dbFeed.Name,
+		URL: dbFeed.Url,
+		UserID: dbFeed.UserID,
+		UserName: dbFeed.UserName,
+	}
+}
