f815f47df595dfca8935b287308dd6663985a7b0
MK <mikalkimmani@gmail.com>
2024-04-06 03:31:27 +0300

    (master): Creation of feeds function

---
+++ handler_feed.go
@@ -0,0 +1,44 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"net/http"
+	"time"
+
+	"github.com/google/uuid"
+	"github.com/mike-kimani/rssagg/internal/database"
+)
+
+func (apiCfg *apiConfig)handlerCreateFeed(w http.ResponseWriter, r *http.Request, user database.User) {
+
+	type parameters struct{
+		Name string `json:"name"`
+		URL string `json:"url"`
+	}
+
+	decoder := json.NewDecoder(r.Body)
+
+	params := parameters{}
+	err := decoder.Decode(&params)
+	if err != nil {
+		respondWithError(w, 400, fmt.Sprintf("Error parsing json: %v", err))
+		return
+	}
+
+	feed, err:= apiCfg.DB.CreateFeeds(r.Context(),database.CreateFeedsParams{
+		ID: uuid.New(),
+		CreatedAt: time.Now().UTC(),
+		UpdatedAt: time.Now().UTC(),
+		Name: params.Name,
+		Url: params.URL,
+		UserID: user.ID,
+		UserName: user.Name,
+	} )
+	if err != nil {
+		respondWithError(w, 400, fmt.Sprintf("Couldn't create feed: %v", err))
+		return
+	}
+
+	respondWithJson(w, 201, databaseUserToFeed(feed))
+}
