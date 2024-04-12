ab9451a056e7effaee514cdf103c33fad8f8e7fc
MK <mikalkimmani@gmail.com>
2024-04-12 14:17:01 +0300

    (master): Convert database feed follows to custom feed follows

--- internal/database/models.go
+++ internal/database/models.go
@@ -21,6 +21,14 @@ type Feed struct {
 	UserName  string
 }
 
+type FeedFollow struct {
+	ID        uuid.UUID
+	CreatedAt time.Time
+	UpdatedAt time.Time
+	UserID    uuid.UUID
+	FeedID    uuid.UUID
+}
+
 type User struct {
 	ID            uuid.UUID
 	CreatedAt     time.Time
