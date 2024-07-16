b6840da8f9b404fa83583addadef30e0882e70ff
MK <muchunumike@gmail.com>
2024-07-16 18:14:25 +0300

    feat(docker_container): Add migrations bash file

---
+++ run_migrations.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+
+set -e
+
+echo "Waiting for postgres to be ready..."
+
+while ! pg_isready -h db -p 5432 ; do
+  echo "still waiting"
+  sleep 2
+done
+
+echo "Running migrations..."
+goose -dir ./sql/schema/ postgres postgres://postgres:novek@db:5432/rssagg up
+
+echo "Migrations completed"
