8f854a238ae2b1059c5ead23a7e41c37383a7803
MK <muchunumike@gmail.com>
2024-08-12 22:43:09 +0300

    feat(standard_layout): Refactor json responses to pkg/

--- pkg/jsonresponses/json.go
+++ pkg/jsonresponses/json.go
@@ -1,13 +1,12 @@
-package main
+package jsonresponses
 
 import (
 	"encoding/json"
 	"log"
 	"net/http"
-	
 )
 
-func respondWithError(w http.ResponseWriter, code int, msg string) {
+func RespondWithError(w http.ResponseWriter, code int, msg string) {
 	if code > 499 {
 		log.Println("Server side error", msg)
 	}
@@ -16,12 +15,12 @@ func respondWithError(w http.ResponseWriter, code int, msg string) {
 		Error string `json:"error"`
 	}
 
-	respondWithJson(w, code, errResponse{
+	RespondWithJson(w, code, errResponse{
 		Error: msg,
 	})
 }
 
-func respondWithJson(w http.ResponseWriter, code int, payload interface{}) {
+func RespondWithJson(w http.ResponseWriter, code int, payload interface{}) {
 	dat, err := json.Marshal(payload)
 	if err != nil {
 		log.Printf("Failed to marshal json response from: %v", payload)
@@ -29,7 +28,7 @@ func respondWithJson(w http.ResponseWriter, code int, payload interface{}) {
 		w.WriteHeader(500)
 		return
 	}
-	w.Header().Add("Content-Type","application/json")
+	w.Header().Add("Content-Type", "application/json")
 	w.WriteHeader(code)
 	/* write the data itself*/
 	w.Write(dat)
