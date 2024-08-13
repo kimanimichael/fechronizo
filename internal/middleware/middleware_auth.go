08cc20bf56092f22e2bfa5cbe3982c0769b17d69
MK <muchunumike@gmail.com>
2024-08-13 09:43:25 +0300

    feat(standard_layout): Refactor middleware to internal/

--- internal/middleware/middleware_auth.go
+++ internal/middleware/middleware_auth.go
@@ -1,26 +1,31 @@
-package main
+package middleware
 
 import (
 	"fmt"
+	"github.com/mike-kimani/fechronizo/pkg/jsonresponses"
 	"net/http"
 
 	"github.com/mike-kimani/fechronizo/internal/database"
 	"github.com/mike-kimani/fechronizo/pkg/auth"
 )
 
+type ApiConfig struct {
+	DB *database.Queries
+}
+
 type authedHandler func(http.ResponseWriter, *http.Request, database.User)
 
-func (apiCfg *apiConfig) middlewareAuth(handler authedHandler) http.HandlerFunc {
+func (apiCfg *ApiConfig) MiddlewareAuth(handler authedHandler) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
 		apiKey, err := auth.GetAPIKey(r.Header)
 		if err != nil {
-			respondWithError(w, 403, fmt.Sprintf("Auth error: %v", err))
+			jsonresponses.RespondWithError(w, 403, fmt.Sprintf("Auth error: %v", err))
 			return
 		}
 		//r.Context() allows using the current context of the request
 		user, err := apiCfg.DB.GetUserByAPIKey(r.Context(), apiKey)
 		if err != nil {
-			respondWithError(w, 400, fmt.Sprintf("Couldn't get user: %v", err))
+			jsonresponses.RespondWithError(w, 400, fmt.Sprintf("Couldn't get user: %v", err))
 			return
 		}
 		handler(w, r, user)
