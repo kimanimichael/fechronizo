2906c4cabd48c2cbc68def73f5b6c1fed86f4636
MK <muchunumike@gmail.com>
2024-08-13 09:58:35 +0300

    feat(standard_layout): Export error Handling function

--- pkg/errors/errors.go
+++ pkg/errors/errors.go
@@ -5,6 +5,6 @@ import (
 	"net/http"
 )
 
-func handlerErr(w http.ResponseWriter, r *http.Request) {
+func HandlerErr(w http.ResponseWriter, r *http.Request) {
 	jsonresponses.RespondWithError(w, 400, "Something went wrong")
 }
