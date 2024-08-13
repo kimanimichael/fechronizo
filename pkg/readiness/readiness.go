e7e40c5af9bb0d1938511e48ab490cd2c803fd3f
MK <muchunumike@gmail.com>
2024-08-13 09:59:11 +0300

    feat(standard_layout): Export readiness http handler

--- pkg/readiness/readiness.go
+++ pkg/readiness/readiness.go
@@ -3,6 +3,6 @@ package readiness
 import "net/http"
 import "github.com/mike-kimani/fechronizo/pkg/jsonresponses"
 
-func handlerReadiness(w http.ResponseWriter, r *http.Request) {
+func HandlerReadiness(w http.ResponseWriter, r *http.Request) {
 	jsonresponses.RespondWithJson(w, 200, struct{}{})
 }
